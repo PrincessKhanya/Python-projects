339317483a68cd430777048cb3a1e45a1e0fdf39
Nokukhanya Makhanya <nokhanyamakhanya@gmail.com>
2021-09-17 18:40:36 +0200

    computor play guess the number

--- guess_random_number.py
+++ guess_random_number.py
@@ -13,6 +13,27 @@ def guess(x):
             print("Sorry, guess again. Too high")
     print(f"Yay, congrat. You have guessed the number {random_number } corretly")
 
+
+def computor_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low!=high:
+            guess = random.randint(low,high)
+        else:
+            guess = high
+        feedback=input(f"is {guess} too high (h), too low (l), correct (c)")
+        if feedback == 'h' :
+            high =guess-1
+        elif feedback == 'l':
+            low=guess+1
+    print(f"The computer guessed the number {guess } corretly")
+
+
+computor_guess(int(input("What range would you like to guess from starting from 1 :")))
+    
+
 guess(int(input("What range would you like to guess from starting from 1 :")))
 
 
